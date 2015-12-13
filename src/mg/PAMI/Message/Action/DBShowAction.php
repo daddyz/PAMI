0f9d7d9052349ffdf10ec9c166ef2c6d2f87d4e3
Vadim Senderovich <vadims@commpeak.com>
2015-12-13 16:03:35 +0200

    one more try

--- src/mg/PAMI/Message/Action/DBShowAction.php
+++ src/mg/PAMI/Message/Action/DBShowAction.php
@@ -33,9 +33,6 @@ class DBShowAction extends OutgoingMessage
         }
         $this->setKey('Action', 'Command');
         $this->setKey('Command', $command);
-
-        //$this->setKey('ActionID', microtime(true));
-
     }
 
     public function handleResponse($raw)
@@ -52,29 +49,4 @@ class DBShowAction extends OutgoingMessage
         return $result;
     }
 
-    /**
-     * Sets Action ID.
-     *
-     * The ActionID can be at most 69 characters long, according to
-     * {@link https://issues.asterisk.org/jira/browse/14847 Asterisk Issue 14847}.
-     *
-     * Therefore we'll throw an exception when the ActionID is too long.
-     *
-     * @param $actionID The Action ID to have this action known by
-     *
-     * @return void
-     * @throws PAMIException When the ActionID is more then 69 characters long
-     */
-    public function setActionID($actionID)
-    {
-        if (0 == strlen($actionID)) {
-            throw new PAMIException('ActionID cannot be empty.');
-        }
-
-        if (strlen($actionID) > 69) {
-            throw new PAMIException('ActionID can be at most 69 characters long.');
-        }
-
-        $this->setKey('ActionID', $actionID);
-    }
 }
--- src/mg/PAMI/Message/Action/ShowChannelsAction.php
+++ src/mg/PAMI/Message/Action/ShowChannelsAction.php
@@ -27,9 +27,6 @@ class ShowChannelsAction extends OutgoingMessage
 
         $this->setKey('Action', 'Command');
         $this->setKey('Command', $command);
-
-        //$this->setKey('ActionID', microtime(true));
-
     }
 
     public function handleResponse($raw)
@@ -48,29 +45,4 @@ class ShowChannelsAction extends OutgoingMessage
         return $result;
     }
 
-    /**
-     * Sets Action ID.
-     *
-     * The ActionID can be at most 69 characters long, according to
-     * {@link https://issues.asterisk.org/jira/browse/14847 Asterisk Issue 14847}.
-     *
-     * Therefore we'll throw an exception when the ActionID is too long.
-     *
-     * @param $actionID The Action ID to have this action known by
-     *
-     * @return void
-     * @throws PAMIException When the ActionID is more then 69 characters long
-     */
-    public function setActionID($actionID)
-    {
-        if (0 == strlen($actionID)) {
-            throw new PAMIException('ActionID cannot be empty.');
-        }
-
-        if (strlen($actionID) > 69) {
-            throw new PAMIException('ActionID can be at most 69 characters long.');
-        }
-
-        $this->setKey('ActionID', $actionID);
-    }
 }
